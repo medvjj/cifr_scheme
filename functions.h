7e3f1d2ea012a9be14203d82e9659d5b6d592525
medvjj <lsmedved@protonmail.com>
2025-01-07 18:44:55 +0800

    second commit

--- functions.h
+++ functions.h
@@ -1,2 +1,2 @@
 int and_2(int a, int b);
-int timer(int a, int b);
+int timer(int a, double b, double c);
--- timer.c
+++ timer.c
@@ -1,9 +1,9 @@
-int timer(int a, int b)
+int timer(int a, double b, double c)
 {
   if (a == 0)
     return 0;
-  if ((a != 0) && (b != 5))
+  if ((a != 0) && (b < c))
     return 0;
-  else //((a != 0) && (b == 5))
+  else //((a != 0) && (b >= 5))
     return 1;
 }
